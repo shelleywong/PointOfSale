158169e9a733998903c8dde27766a86d5e2af8f8
Shelley Wong <swong26@mail.csuchico.edu>
2016-10-31 16:14:21 -0700

    in addItem(), add condition to check if input is a number

--- sales.js
+++ sales.js
@@ -4,8 +4,11 @@ function addItem()
 {
   var newItem;
   newItem = document.getElementById("price").value;
-  //IF newItem is not a number
-  // THEN show an alert: "Enter price as a number"
+  if(isNaN(newItem))//IF newItem is not a number
+  {
+    // THEN show an alert: "Enter price as a number"
+    window.alert("Enter price as a number");
+  }
   //OTHERWISE,
   // update newItem to its value cast as a number
   // update runningTotal to be its value plus newItem
