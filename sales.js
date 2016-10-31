377f7a6656f3d90e951d27f1843098554de4aa30
Shelley Wong <swong26@mail.csuchico.edu>
2016-10-31 16:33:31 -0700

    finished addItem() function

--- sales.js
+++ sales.js
@@ -4,19 +4,28 @@ function addItem()
 {
   var newItem;
   newItem = document.getElementById("price").value;
-  if(isNaN(newItem))//IF newItem is not a number
+  if(isNaN(newItem)) //IF newItem is not a number
   {
     // THEN show an alert: "Enter price as a number"
     window.alert("Enter price as a number");
   }
-  //OTHERWISE,
-  // update newItem to its value cast as a number
-  // update runningTotal to be its value plus newItem
-  // create a variable called dollars
-  // call asCurrency() by with the value of runningTotal and assign the return value to dollars
-  // update the innerHTML of the span with the id "subtotal" to be dollars
-  // update the value of the input with the id "price" to be an empty string
-  // update a cookie called "preTax" with the value of runningTotal
+  else //OTHERWISE,
+  {
+    // update newItem to its value cast as a number
+    newItem = Number(newItem);
+    // update runningTotal to be its value plus newItem
+    runningTotal += newItem;
+    // create a variable called dollars
+    var dollars;
+    // call asCurrency() by with the value of runningTotal and assign the return value to dollars
+    dollars = asCurrency(runningTotal);
+    // update the innerHTML of the span with the id "subtotal" to be dollars
+    document.getElementById("subtotal").innerHTML = dollars;
+    // update the value of the input with the id "price" to be an empty string
+    document.getElementById("price").innerHTML = "";
+    // update a cookie called "preTax" with the value of runningTotal
+    setCookie(preTax,runningTotal,1);
+  }
 }
 
 //takes a number and gives a string with the number displayed as USD currency
